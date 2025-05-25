290f5c9aa309dff47e0c8d80549ee97982bf2e84
NasimRanaFeroz <feroznasimrana@gmail.com>
2025-05-25 21:43:08 +0800

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/Navbar";
-
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
-import Orders from "./pages/Orders";
+import Dashboard from "./pages/Dashboard";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
-import Login from "./pages/Login";
-
-
-import{Routes, Route} from 'react-router-dom';
 import PlaceOrders from "./pages/PlaceOrders";
 import Collection from "./pages/Collection";
+import Login from "./pages/Login";
+import Orders from "./components/Orders";
 import Footer from "./components/Footer";
+import Profile from "./components/Profile";
+import EditProducts from "./components/EditProducts";
+import AllProducts from "./components/AllProducts";
+import Users from "./components/Users";
 
 export default function App() {
   return (
@@ -22,6 +24,13 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route index element={<Profile />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="all-products" element={<AllProducts />} />
+          <Route path="edit-products" element={<EditProducts />} />
+          <Route path="users" element={<Users />} />
+        </Route>
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/collection" element={<Collection />} />
@@ -34,8 +43,6 @@ export default function App() {
       </Routes>
 
       <Footer />
-      
     </div>
-   
-  )
+  );
 }
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,70 +1,56 @@
-
-import { useContext, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-
-import { ShopContext } from '../context/ShopContext'
-
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
 const Product = () => {
-  const [productData, setProductData] = useState(false)
-  const [image, setImage] = useState('')
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState("");
 
-  const {productId} = useParams()
+  const { productId } = useParams();
 
-  const {products} = useContext(ShopContext)
+  const { products } = useContext(ShopContext);
   // console.log(products)
 
   const fetchProduct = async () => {
-    products.map(product => {
-      if(product._id === productId) {
-        setProductData(product)
-        setImage(product.image[0])
-        return null
+    products.map((product) => {
+      if (product._id === productId) {
+        setProductData(product);
+        setImage(product.image[0]);
+        return null;
       }
-    })
-  }
-
+    });
+  };
 
   useEffect(() => {
+    fetchProduct();
+  }, [productId]);
 
-    fetchProduct()
-    
-  }, [productId])
-
-
-
-
-  
-  return productData ?(
-    <div className='transition-opacity ease-in-out duration-100 opacity-100 mt-10 px-4  md:px-[6vw] lg:px-[8vw]'>
-
+  return productData ? (
+    <div className="transition-opacity ease-in-out duration-100 opacity-100 mt-10 px-4  md:px-[6vw] lg:px-[8vw]">
       {/* product data */}
-      <div className='flex flex-col sm:flex-row  sm:gap-12  gap-2'>
-
+      <div className="flex flex-col sm:flex-row  sm:gap-12  gap-2">
         {/* product image */}
-        <div className='flex-1 flex flex-col-reverse sm:flex-row'>
-            <div className='flex justify-between overflow-x-auto w-full sm:flex-col gap-2 sm:overflow-y-scroll sm:justify-normal sm:w-[19%]'>
-                {
-                  productData.image.map((img, index) => (
-                    
-                      <img onClick={()=>setImage(img)} key={index}  src={img} alt="image" className='w-[24%] flex-shrink-0 sm:w-full cursor-pointer'/>
-                   
-                  ))
-                }
-            </div>
-            <div className='w-full sm:w-[80%]'>
-              <img src={image} alt="product" className='w-full h-auto '/>
-            </div>
-            <div className='flex-1 bg-emerald-400'>
-              <p className='text-center text-white'>Product Image</p>
-
-            </div>
+        <div className="flex-1 flex flex-col-reverse sm:flex-row">
+          <div className="flex justify-between overflow-x-auto w-full sm:flex-col gap-2 sm:justify-normal sm:w-[19%]">
+            {productData.image.map((img, index) => (
+              <img
+                onClick={() => setImage(img)}
+                key={index}
+                src={img}
+                alt="image"
+                className="w-[47%] h-auto sm:object-cover cursor-pointer"
+              />
+            ))}
+          </div>
+          <div className="w-[37%] ">
+            <img src={image} alt="product" className="w-full h-auto " />
+          </div>
         </div>
-
       </div>
-      
     </div>
-  ): <div className='opacity-0'></div>
-}
+  ) : (
+    <div className="opacity-0"></div>
+  );
+};
 
-export default Product
+export default Product;
